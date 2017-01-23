b1066984e2b4319c54931e9dc98f9247210dc12a
Ramb0t <rambo@rambo.co.za>
2017-01-23 18:14:25 +0200

    Starting to split into multiple files

---
+++ CameraSlider/CameraSliderFimwareV2/src/Timer1.cpp
@@ -0,0 +1,45 @@
+#include "Timer1.h"
+
+// Functions:
+/*****************************************************************************/
+// ISR to do stepper moves
+void timerIsr() {
+
+  debug_ticker++;
+  if(debug_ticker >= DEBUG_TICKS){
+    Serial.print("DEBUG: ");
+    Serial.print("Run ");
+    Serial.print(running);
+    Serial.print(", Spd ");
+    Serial.print(actual_speed);
+    Serial.print(", Dir ");
+    Serial.print(actual_direction);
+    Serial.print(", MaxF ");
+    Serial.print(MAX_FLAG);
+    Serial.print(", MinF ");
+    Serial.print(MIN_FLAG);
+    Serial.print(", Stat ");
+    Serial.println(status);
+
+  }
+
+  //Don't even bother
+  if(actual_speed == 0) return;
+
+  // inc the step counter
+  tick_count++;
+
+  // if we have hit the limit and we should be running
+  if((tick_count >= ticks) && running) {
+
+    // make a step
+    digitalWrite(SSTP, HIGH);
+    digitalWrite(SSTP, LOW);
+
+    // inc the step_counter
+    step_count++;
+
+    // reset tick counter
+    tick_count = 0;
+  }
+}
